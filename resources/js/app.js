117587389866ccfad31210571714f0aead71537d
khalilo2ya <khalilo2y@gmail.com>
2022-04-01 22:44:58 +0100

    done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,11 @@
 require('./bootstrap');
 
-window.Vue = require('vue').default;
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+window.Vue = require('vue').default;
+Vue.use(ElementUI);
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('employee-form', require('./components/employee/EmployeeForm.vue').default);
 
