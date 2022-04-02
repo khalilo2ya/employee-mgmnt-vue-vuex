4cb99a2bf8fc14febebae2017f3a05cf13ddb16d
khalilo2ya <khalilo2y@gmail.com>
2022-04-02 12:14:54 +0100

    first from vue

--- resources/js/components/employee/EmployeeForm.vue
+++ resources/js/components/employee/EmployeeForm.vue
@@ -2,27 +2,72 @@
   <div class="container">
     <el-card class="box-card">
       <div slot="header" class="clearfix">
-          hello this is card
-            <el-page-header @back="goBack" content="detail">
-            </el-page-header>
+        <el-page-header @back="goBack" content="detail"> </el-page-header>
       </div>
       <div>
-          <el-form ref="employeeForm" :model="form" label-width="120px">
-              <el-row :guuter="10">
-
-
-              </el-row>
-          </el-form>
+        <el-form ref="employeeForm" :model="form" label-width="120px">
+          <el-row :guuter="10">
+            <el-form-item label="Name" required prop="name">
+              <el-input
+                v-model="form.name"
+                aria-placeholder="Employee Name"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="Department" required prop="department">
+              <el-input
+                v-model="form.department"
+                aria-placeholder="Department"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="Section" required prop="section">
+              <el-input
+                v-model="form.section"
+                aria-placeholder="Section"
+              ></el-input>
+            </el-form-item>
+            <el-form-item label="Email" required prop="email">
+              <el-input
+                v-model="form.email"
+                aria-placeholder="Email Adress"
+              ></el-input>
+            </el-form-item>
+          </el-row>
+          <el-row :guuter="10">
+            <el-form-item>
+              <el-button type="success" @click="saveForm('employeeForm')"
+                >Save</el-button
+              >
+            </el-form-item>
+          </el-row>
+        </el-form>
       </div>
-
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
+  name: "employee-form",
   mounted() {
     console.log("Component mounted.");
+  },
+  data() {
+    return {
+      form: {
+        name: null,
+        department: null,
+        section: null,
+        email: null,
+      },
+    };
+  },
+  methods: {
+    goBack() {
+      window.location.href = "/employees";
+    },
+    saveForm(formName) {
+      alert(formName);
+    },
   },
 };
 </script>
